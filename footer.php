b855f1bd48a07512c393231b5c7d02a600f5b57e
Pol Solà <psola@doonamis.es>
2016-12-19 13:41:30 +0100

    Included off-canvas navigation & footer credits

--- footer.php
+++ footer.php
@@ -13,6 +13,32 @@
  */
 
 ?>
+				</main>
+				<footer class="footer">
+					<div class="footer__credits">
+						<div class="row">
+							<div class="medium-6 columns">
+								© <?php echo date('Y'); ?> <?php _e('All rights reserved', 'templet'); ?>
+							</div>
+							<div class="medium-6 columns">
+								<nav class="footer__credits__nav">
+	                <?php
+	                wp_nav_menu( array(
+	                    'sort_column'     => 'menu_order',
+	                    'container'       => false,
+	                    'menu_class'      => 'menu',
+	                    'theme_location'  => 'footer',
+	                    'items_wrap'      => '<ul id="%1$s" class="%2$s" data-dropdown>%3$s</ul>'
+	                    )
+	                );
+	                ?>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</footer>
+			</div><!-- END .off-canvas-content -->
+		</div><!-- END .off-canvas-wrapper -->
 	<?php wp_footer(); ?>
 	</body>
 </html>
